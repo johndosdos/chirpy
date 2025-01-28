0083f7b5180f9c81c1db4163cd3240436038f972
John <128350391+alpaOo@users.noreply.github.com>
2025-01-28 20:46:13 +0800

    feat: add readiness endpoint to assess server health

---
+++ internal/app/chirpy/handlers/health/health.go
@@ -0,0 +1,30 @@
+package health
+
+import (
+	"log"
+	"net/http"
+)
+
+func Check() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		/*
+			http.ResponseWriter handles the response that our server sends back
+			to the client.
+		*/
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		/*
+			technically, the error won't be returned to the client after w.Write().
+			it's just here for logging purposes.
+
+			it's a good practice tho.
+		*/
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Printf("failed to write healthz response: %v", err)
+		}
+	})
+}
