de7f9561467b77bd151ef84501ec2c3f82856c44
John <128350391+alpaOo@users.noreply.github.com>
2025-01-30 00:35:42 +0800

    feat: integrate newly added handlers to main

--- cmd/chirpy/main.go
+++ cmd/chirpy/main.go
@@ -5,17 +5,25 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/johndosdos/chirpy/internal/app/chirpy"
 	"github.com/johndosdos/chirpy/internal/app/chirpy/handlers/health"
+	"github.com/johndosdos/chirpy/internal/app/chirpy/handlers/metric"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &chirpy.ApiConfig{}
 
 	// check file server readiness.
 	health.Check(mux)
 
+	// strip the prefix "/app/" from the URL path for proper routing.
+	// URL path != file path on the server.
 	fileServer := http.StripPrefix("/app/", http.FileServer(http.Dir("web/")))
-	mux.Handle("/app/", fileServer)
+
+	mux.Handle("/app/", apiCfg.MiddlewareMetricsInc(fileServer))
+	mux.Handle("/metrics", metric.GetHits(apiCfg))
+	mux.Handle("/reset", metric.ResetMetrics(apiCfg))
 
 	server := http.Server{
 		Addr:    ":8080",
