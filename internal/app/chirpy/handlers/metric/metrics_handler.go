f036f9807f70821261a890555c6f99d83cf14b79
John <128350391+alpaOo@users.noreply.github.com>
2025-01-31 00:03:20 +0800

    feat: serve html instead of plain text for server hits

--- internal/app/chirpy/handlers/metric/metrics_handler.go
+++ internal/app/chirpy/handlers/metric/metrics_handler.go
@@ -9,11 +9,24 @@ import (
 
 func GetHits(cfg *chirpy.ApiConfig) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 
 		serverHits := cfg.FileserverHits.Load()
-		fmt.Fprintf(w, "Hits: %d\n", serverHits)
+		fmt.Fprintf(w, `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+    </body>
+</html>
+		`, serverHits)
 	})
 }
 
