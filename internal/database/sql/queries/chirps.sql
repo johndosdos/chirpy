57b9ef7efa732e4deb2a56a4b3638ff541aa54fa
John <128350391+alpaOo@users.noreply.github.com>
2025-02-04 19:37:02 +0800

    feat(db): insert chirp into chirps table, and sqlc

---
+++ internal/database/sql/queries/chirps.sql
@@ -0,0 +1,8 @@
+-- name: AddChirp :one
+INSERT INTO chirps (
+    id, created_at, updated_at, body, user_id
+)
+VALUES (
+    gen_random_uuid(), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, $1, $2
+)
+RETURNING *;
