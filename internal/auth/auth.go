129e01ed8e13a3fb6a5ca0a532e513dfaee30f1d
John <128350391+alpaOo@users.noreply.github.com>
2025-02-20 14:01:53 +0800

    feat(auth): implement JWT validation

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -36,4 +38,48 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 	})
 
 	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	if tokenString == "" || tokenSecret == "" {
+		return uuid.Nil, errors.New("token string or token secret is empty")
+	}
+
+	// issuer is the one issuing the JWT, usually the server.
+	//
+	// subject is usually who the JWT is intended for.
+	//
+	// audience is which resource server or API the JWT is intended for.
+	// subject is within the audience.
+
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&jwt.RegisteredClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Method.Alg())
+			}
+
+			return []byte(tokenSecret), nil
+		},
+		jwt.WithIssuer("chirpy"),
+		jwt.WithTimeFunc(time.Now().UTC))
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to parse token: %w", err)
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.Nil, jwt.ErrInvalidType
+	}
+	if claims.Subject == "" {
+		return uuid.Nil, jwt.ErrTokenInvalidSubject
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("%v. invalid subject format: %w", jwt.ErrTokenInvalidSubject, err)
+	}
+
+	return userID, nil
 }
