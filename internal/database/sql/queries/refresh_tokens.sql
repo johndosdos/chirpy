f010969f5517c37fe935bc43c5e064c8e129369b
John <128350391+alpaOo@users.noreply.github.com>
2025-03-01 17:49:18 +0800

    refactor(db): cast revoked_at null value as timestamp with time zone

--- internal/database/sql/queries/refresh_tokens.sql
+++ internal/database/sql/queries/refresh_tokens.sql
@@ -3,6 +3,6 @@ INSERT INTO refresh_tokens (
     token, created_at, updated_at, user_id, expires_at, revoked_at
 )
 VALUES (
-    $1, $2, $3, $4, $5, NULL
+    $1, $2, $3, $4, $5, CAST(NULL AS TIMESTAMP WITH TIME ZONE)
 )
 RETURNING *;
