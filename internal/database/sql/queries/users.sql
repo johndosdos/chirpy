a08bac4318391a2f5f2052f8f4dad380a4d45e8b
John <128350391+alpaOo@users.noreply.github.com>
2025-03-03 14:18:00 +0800

    feat(db): add query to update user info

--- internal/database/sql/queries/users.sql
+++ internal/database/sql/queries/users.sql
@@ -11,3 +11,9 @@ DELETE FROM users;
 -- name: GetUserByEmail :one
 SELECT * FROM users
 WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET updated_at = CURRENT_TIMESTAMP, email = $1, hashed_password = $2
+WHERE id = $3
+RETURNING *;
